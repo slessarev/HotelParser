5989e60b7ab1d82542432a1d612b00d39760af60
Aleksandr Slesarev <slessarevalex@ya.ru>
2023-03-30 19:21:27 +0300

    Записал данные в файл. но еще не финал

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import pars
 import requests
 from bs4 import BeautifulSoup as BS
+import csv
 
 
 
@@ -8,5 +9,14 @@ from bs4 import BeautifulSoup as BS
 
 ls_url = pars.get_url_list(1)
 
-for item in ls_url:
-    print(pars.get_data(item))
+# for item in ls_url:
+#     print(pars.get_data(item))
+
+
+with open('data.csv', 'a', newline='') as file:
+    writer = csv.writer(file)
+    # записываем каждую строку из списка в файл
+    for item in ls_url:
+        data_list = pars.get_data(item)
+        print(data_list)
+        writer.writerow(data_list)
